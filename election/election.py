372c69736e36123e5ffe61f54bed668814f6d2a7
Saravanan <bprathap104@gmail.com>
2020-10-11 19:41:51 +0530

    first commit

---
+++ election/election.py
@@ -0,0 +1,37 @@
+from abc import ABC
+
+class IElection(ABC):
+    """
+    In this class, we define the methods & attributes required for an election    
+    """
+    pass
+
+class Campaign:
+    """
+    In this calss, we define the methods & attributes required for an Election camapign
+    """
+    pass
+
+class Voting:
+    """
+    In this calss, we define the methods & attributes required for an Election voting
+    """
+    pass
+
+class Counting:
+    """
+    In this calss, we define the methods & attributes required for an Election counting
+    """
+    pass
+
+class StateElection(IElection):
+    """
+    In this calss, we define the methods & attributes required for a StateElection
+    """
+    pass
+
+class CentralElection(IElection):
+    """
+    In this calss, we define the methods & attributes required for a CentralElection
+    """
+
---
+++ election/party.py
@@ -0,0 +1,16 @@
+from citizen import Citizen
+class Party:
+    """
+    In this class, we defines attributes & methods for a party 
+    """
+    pass
+class Candidate(Citizen):
+    """
+    In this class, we define attributes & methods for a candidate
+    """
+    pass
+class Voter(Citizen):
+    """
+    In this class, we define attributes & methods for a voter
+    """
+    pass
