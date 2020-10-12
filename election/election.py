ed8a73fe6ffdf019ffc0c855b5ff87677bcff6f0
Saravanan <bprathap104@gmail.com>
2020-10-12 20:20:48 +0530

    added some more features for state election

--- election/election.py
+++ election/election.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 from datetime import timedelta  
+import csv
 
 class IElection(ABC):
     """
@@ -13,6 +14,9 @@ class Campaign:
     """
     def __init__(self, election_date):
         self.date = convert_string_to_date(election_date)
+    def __repr__(self):
+        campaign_end_date = self.date - timedelta(days=3) 
+        return(f'{campaign_end_date}')
     def __str__(self):
         campaign_end_date = self.date - timedelta(days=3) 
         return(f'The Campaign End Date is : {campaign_end_date}')
@@ -23,8 +27,11 @@ class Voting:
     """
     def __init__(self, election_date):
         self.date = convert_string_to_date(election_date)
+    def __repr__(self):
+        return(f'{self.date}')
     def __str__(self):
-        return(f'The Election Date is : {self.date}')
+        vote_counting_date = self.date + timedelta(days=3) 
+        return(f'The Election Date is : {vote_counting_date}')
 
 class Counting:
     """
@@ -32,6 +39,9 @@ class Counting:
     """
     def __init__(self, election_date):
         self.date = convert_string_to_date(election_date)
+    def __repr__(self):
+        vote_counting_date = self.date + timedelta(days=3) 
+        return(f'{vote_counting_date}')
     def __str__(self):
         vote_counting_date = self.date + timedelta(days=3) 
         return(f'The Counting Date is : {vote_counting_date}')
@@ -41,15 +51,28 @@ class StateElection(IElection):
     """
     In this calss, we define the methods & attributes required for a StateElection
     """
-    def __init__(self, election_date):
+    def __init__(self, state_code, state_name, election_date):
         self.date = election_date
+        self.state_code = state_code
+        self.state_name = state_name
         self.campaign = Campaign(self.date)
         self.voting = Voting(self.date)
         self.counting = Counting(self.date)
+
     def display_election_details(self):
         print(self.voting)
         print(self.campaign)
         print(self.counting)
+    
+    def write_to_csv(self):
+        vote_counting_date=repr(self.counting)
+        campaign_end_date=repr(self.campaign)
+        with open('state_election.csv', 'a',newline='\n') as se_file:
+            writer = csv.writer(se_file)
+            writer.writerow([self.state_code, self.state_name, self.date, campaign_end_date, vote_counting_date])
+        se_file.close()
+
+
 
 class CentralElection(IElection):
     """
--- main.py
+++ main.py
@@ -3,14 +3,31 @@ from election.citizen import Citizen
 from election.party import Party
 import json
 
+def display_states():
+    with open('state.json') as state_file:
+        state_json = json.load(state_file)
+    counter = 1
+    for state in state_json['states']:
+        print(f'{state["state_id"]}: {state["state_name"]}')
+    state_code = input("Enter State Code:")
+    state_name_list = list(filter(lambda state:(state["state_id"]==state_code), state_json['states']))
+    if len(state_name_list) > 0:
+        state_name = state_name_list[0].get('state_name')
+    else:
+        print(f'Enter proper state code')
+        exit(0)
+    state_election_date = input("Enter Election Date in YYYY-MM-DD:")
+    print(f'{state_code},{state_name}')
+    se = StateElection(state_code, state_name, state_election_date)
+    se.display_election_details()
+    se.write_to_csv()
+
 def handle_input():
     choice = input("Choice: ")
     if choice == "3":
         return False
     if(choice == "1"):
-        state_election_date = input("Enter Election Date in YYYY-MM-DD:")
-        se = StateElection(state_election_date)
-        se.display_election_details()
+        display_states()
     elif(choice == "2"):
         central_election_date = input("Enter Election Date in YYYY-MM-DD:")
         ce = CentralElection(central_election_date)
